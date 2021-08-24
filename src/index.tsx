46e9574bfde0106bb30aad9412193bbf1cbe5e36
Si9num <78493168+Si9num@users.noreply.github.com>
2021-08-24 21:16:43 +0300

    Delete src directory

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
-
-import searchBar from './searchBar';
-import renderCard from './cards';
-
-ReactDOM.render(searchBar(), document.querySelector('#root'));
-ReactDOM.render(renderCard(), document.querySelector('.card-container'));
